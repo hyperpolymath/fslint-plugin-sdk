24abd58be5574e9270116c076a90ec954378177b
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2025-12-12 14:15:16 +0000

    Add RSR standard files (compliance, metadata, build)

---
+++ STATE.scm
@@ -0,0 +1,15 @@
+;; STATE.scm - Project State
+;; SPDX-License-Identifier: MIT
+
+(project
+  (name "fslint-plugin-sdk")
+  (version "0.1.0")
+  (status "active")
+  (language "rust")
+  (tier 1)
+  (description "SDK for building fslint plugins")
+  (repository "https://github.com/hyperpolymath/fslint-plugin-sdk")
+  (author "Jonathan D.A. Jewell")
+  (license "MIT")
+  (created "2025-12-12")
+  (rsr-compliant #t))
